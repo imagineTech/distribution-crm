42e3cadb18ef53335673ca31268807ba647b7269
Matt Mortensen <mortensenm1205@gmail.com>
2018-05-18 18:25:28 -0600

    rest of app

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SignUp from '../components/signup';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SignUp />
+      </div>
+    );
+  }
+}
+
+export default App;
