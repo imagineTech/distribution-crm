95623544c6bac41128fbf4c3fa7fdc119b2546d6
Matt Mortensen <mortensenm1205@gmail.com>
2018-05-18 20:52:20 -0600

    brought in placeholder files for jays home page / find out more page, he will fill in later

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store/configStore';
+import App from './containers/App_signUp';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = configureStore();
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
