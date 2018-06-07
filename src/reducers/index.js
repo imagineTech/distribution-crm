f1dcee5417f397fdf37a6116bdc2b36c695a6d3c
Matt Mortensen <mortensenm1205@gmail.com>
2018-06-06 23:21:39 -0600

    restarting fb login auth

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import { emailAndPasswordData, signUpFormData } from './signUpData';
+import { emailAndPasswordToState } from './loginAuth';
 
 export default combineReducers({
   emailAndPasswordData,
-  signUpFormData
+  signUpFormData,
+  emailAndPasswordToState
 });
