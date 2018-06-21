e91df5583ae526f2059cbcf89706df3c16a2b03c
Matt Mortensen <mortensenm1205@gmail.com>
2018-06-20 22:45:11 -0600

    refactored to now hold Route

--- src/containers/App_signUp.js
+++ src/containers/App_signUp.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Crm from './CRM';
 import SignUp from '../components/Signup';
 import Login from '../components/Login';
+import * as routes from '../constants/routes';
+
+const Landing = () => {
+  return (
+    <div>
+      <SignUp />
+      <Login />
+    </div>
+  );
+}
 
 class App extends Component {
   render() {
     return (
       <div>
-        <SignUp />
-        <Login />
+        <Route exact path={routes.LANDING} component={() => <Landing />} />
+        <Route exact path={routes.CRM} component={() => <Crm />}  />
       </div>
-    );
+  )
   }
 }
 
