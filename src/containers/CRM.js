5f4c579f81b8df97b458ccfc7d9095f3bc9cb5c0
Matt Mortensen <mortensenm1205@gmail.com>
2018-06-25 21:58:17 -0600

    refactored Crm component to hold redux calls for logout

--- src/containers/CRM.js
+++ src/containers/CRM.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { signOutAuth } from '../actions/signOut';
 
-const Crm = () => {
-  return <h1>Welcome to our Crm</h1>
+class Crm extends Component {
+
+  handleSignOut = () => {
+    this.props.signOut();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome to our Crm</h1>
+        <button onClick={this.handleSignOut}>Sign Out</button>
+      </div>
+    );
+  };
 }
 
-export default Crm;
+const mapStateToProps = state => {
+  return {};
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    signOut: () => dispatch(signOutAuth())
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Crm);
