68d6c92d733338390f2d7be66e03652db8c11f4f
Matt Mortensen <mortensenm1205@gmail.com>
2018-06-30 12:40:46 -0600

    keep refactoring to have findoutmore compt appear consistently

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,21 +3,12 @@ import styled from 'styled-components';
 import '../App.css'
 
 //Custom components
-// import { FindOutMore } from './components/findOutMore/FindOutMore';
-import Hamburger from '../components/partials/hamburger/Hamburger';
-// import FindoutLink from './components/Navigation';
 import { Route } from 'react-router-dom';
-// import {FindoutHeader} from './components/findOutMore/FindoutHeader';
 import Landing from '../components/landing/Landing';
 import FindoutLanding from '../components/findoutmore/FindoutLanding';
-import { LandingLinks } from '../components/Navigation';
 import About from '../components/findoutmore/subcomponents/about/About';
 import OurPolicy from '../components/findoutmore/subcomponents/ourpolicy/OurPolicy';
 import Contact from '../components/findoutmore/subcomponents/contact/Contact';
-
-
-
-
 import * as routes from '../constants/routes';
 
 
@@ -25,14 +16,11 @@ class App extends Component {
   render() {
     return (
         <div id="main-container">
-            {/* <Route path="/" component={Landing} exact /> */}
-            <Route path={routes.HOME} component={Landing} exact />
-            <Route path={routes.FIND_OUT_MORE} component={FindoutLanding} />
-            <Route path={routes.ABOUT} component={About}  />
-            <Route path={routes.OUR_POLICY} component={OurPolicy} />
-            <Route path={routes.CONTACT} component={Contact} />
-
-            {/* <Route path={routes.HOME} component={About} exact /> */}
+            <Route exact path={routes.HOME} component={() => <Landing />}  />
+            <Route exact path={routes.FIND_OUT_MORE} component={() => <FindoutLanding />} />
+            <Route exact path={routes.ABOUT} component={() => <About />}  />
+            <Route exact path={routes.OUR_POLICY} component={() => <OurPolicy />} />
+            <Route exact path={routes.CONTACT} component={() => <Contact />} />
         </div>
     );
   }
