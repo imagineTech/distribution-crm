ff6090746085a3c35bbcd1b2adf11ac7d42810b1
Matt Mortensen <mortensenm1205@gmail.com>
2018-06-30 10:04:34 -0600

    adding comments of purpose for action directory

--- src/actions/signOut.js
+++ src/actions/signOut.js
@@ -1,3 +1,9 @@
+/*
+  File is pretty straight forward, only used
+  for signing a user out. It will also redirect
+  them
+*/
+
 import { auth } from '../firebase/index';
 import { push } from 'connected-react-router';
 import * as routes from '../constants/routes';
