f577644cfd1bc672737f234133b0ad98bf891877
Matt Mortensen <mortensenm1205@gmail.com>
2018-08-11 08:49:25 -0600

    refactored to be a stateless co for non leaks

--- src/components/CRM/Product/ProductItem.js
+++ src/components/CRM/Product/ProductItem.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-const ProductItem = () => {
-  return (
-    <div>
-      <h1>Product</h1>
-    </div>
-  )
-}
+const ProductItem = ({products}) => {
+    return (
+      <div>
+        <h1>Product</h1>
+        {products.map(product => {
+          return (
+            <div key={product.id}>
+              <p>{product.name}</p>
+              <p>{product.description}</p>
+            </div>
+          );
+        })}
+        {console.log(products)}
+      </div>
+    )
+};
 
 export default ProductItem;
