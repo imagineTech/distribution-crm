2e2c93ff5385b9e8ebad7b79cb3920daeab6fe71
Matt Mortensen <mortensenm1205@gmail.com>
2018-08-14 18:27:33 -0600

    bringing a standalone product page

---
+++ src/components/CRM/Product/subcomponents/ProductItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductItem = ({ product, match }) => {
+    return (
+      <div>
+        <h1>hello</h1>
+      </div>
+    )
+};
+
+export default ProductItem;
