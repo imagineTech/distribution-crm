950a245815b43e0e6c37fcb98224e39163357087
Matt Mortensen <mortensenm1205@gmail.com>
2018-08-18 09:40:55 -0600

    refactor for async/await

--- src/moltin/index.js
+++ src/moltin/index.js
@@ -1,5 +1,6 @@
 import Moltin  from './config_moltin';
 
-export const getAllProducts = () => {
-  return Moltin.Products.All();
+export const getAllProducts = async () => {
+  const products = await Moltin.Products.All();
+  return products;
 }
