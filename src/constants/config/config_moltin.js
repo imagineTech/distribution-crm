1754deb2963888ced8b543912e88e185ebf1ed64
Matt Mortensen <mortensenm1205@gmail.com>
2018-08-23 17:21:53 -0600

    ignoring config files

--- src/constants/config/config_moltin.js
+++
@@ -1,8 +0,0 @@
-import { gateway as MoltinGateway } from '@moltin/sdk';
-
-const Moltin = MoltinGateway({
-  client_id: 'MITUh1nVIIM2mCFEd5beiS3CLJhL3JSs8TW6JJmiJq',
-  client_secret: 'Biydyj0jR9JJ0onNnOWShWnh0vkGQQnZJjMdjFMfLe'
-});
-
-export default Moltin;
