26d4df764032f302b7678819634d177515b1d499
Matt Mortensen <mortensenm1205@gmail.com>
2018-08-23 17:05:51 -0600

    adding more to constants

---
+++ src/constants/config/config_moltin.js
@@ -0,0 +1,8 @@
+import { gateway as MoltinGateway } from '@moltin/sdk';
+
+const Moltin = MoltinGateway({
+  client_id: 'MITUh1nVIIM2mCFEd5beiS3CLJhL3JSs8TW6JJmiJq',
+  client_secret: 'Biydyj0jR9JJ0onNnOWShWnh0vkGQQnZJjMdjFMfLe'
+});
+
+export default Moltin;
