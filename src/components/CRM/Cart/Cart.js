87139529f94e81d77ea4bfe2f1754985e3f5d1f3
Matt Mortensen <mortensenm1205@gmail.com>
2018-08-22 20:52:48 -0600

    update Cart from error

--- src/components/CRM/Cart/Cart.js
+++ src/components/CRM/Cart/Cart.js
@@ -1,43 +1,11 @@
 import React from 'react';
-// import ProductItems from '../../Product/subcomponents/ProductItems';
-
-// const Moltin = MoltinGateway({
-//   client_id: '{ authUser.uid }'
-// })
-// const referenceId= '{ cart.cartItems.id }'
-
-// const Cart = (cart, cartItems, products) => {
-//   return (
-//     <div>
-//       <h1>Hello</h1>
-//       <div key = { cart.cartItems[id] }>
-// 	     { products.map(product => {
-// 	     	<div key = { product.id }>
-// 	     		<h2> {product.name }</h2>
-// 	     		<h2> {product.description }</h2>
-// 	     	</div>
-// 	     })}
-//       </div>
-//     </div>
-//   );
-// } 
-const MoltinGateway = require('@moltin/sdk').gateway
-
-const Moltin = MoltinGateway({
-  client_id: '{ authUser.uid }'
-})
-
-const reference = '{  }'
-
-Moltin.cart( reference )
-	.CartItems()
-	.then(cart => {
-		console.log(CartItems.all)
-	})
 
 const Cart = () => {
-
+  return(
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
 }
-
 
 export default Cart;
