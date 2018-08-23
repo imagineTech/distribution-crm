058c6071a7384e8d6e7ad54748085d5556a298f3
Matt Mortensen <mortensenm1205@gmail.com>
2018-08-23 17:09:15 -0600

    updating for ducks dir

--- src/moltin/index.js
+++ src/moltin/index.js
@@ -1,20 +1,5 @@
 import Moltin from './config_moltin';
 
-export const getAllProducts = async () => {
-  const products = await Moltin.Products.All();
-  return products;
-}
-
-export const addProductsToCart = async (refId, productId, productQuantity) => {
-  const cart = await Moltin.Cart(refId).AddProduct(productId, productQuantity);
-  return cart;
-}
-
-export const updateItemQuantity = async (refId, itemId, newQuantity) => {
-  const update = await Moltin.Cart(refId).UpdateItemQuantity(itemId, newQuantity);
-  return update;
-}
-
 export const getACart = async (refId) => {
   const cart = await Moltin.Cart(refId).Get();
   return cart;
