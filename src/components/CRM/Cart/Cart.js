5f960463545a2169809245b0c5e2b878131f68bf
Matt Mortensen <mortensenm1205@gmail.com>
2018-08-23 21:29:14 -0600

    cart itemsnow display

--- src/components/CRM/Cart/Cart.js
+++ src/components/CRM/Cart/Cart.js
@@ -2,13 +2,34 @@ import React, { Component } from 'react';
 import * as Moltin from '../../../moltin/index';
 
 class Cart extends Component {
+
+  state = {
+    cartItems: []
+  };
+
   componentDidMount() {
-    Moltin.getACart(this.props.auth.uid).then(cart => console.log(cart));
+    const { auth } = this.props;
+    Moltin.getACart(auth.uid).then(cart => {
+      this.setState({
+        cartItems: this.state.cartItems.concat(cart.data)
+      })
+    });
   }
   render() {
+    const { cartItems } = this.state;
+    const { auth } = this.props;
     return(
       <div>
-        <h1>Hello</h1>
+        {cartItems.map(item => {
+          return(
+            <div key={item.id}>
+              <span>{item.quantity}</span>
+              <span>{item.name}</span>
+              <span>{item.meta.display_price.with_tax.unit.formatted}</span>
+            </div>
+          )
+        })}
+        {console.log(cartItems)}
       </div>
     )
   }
