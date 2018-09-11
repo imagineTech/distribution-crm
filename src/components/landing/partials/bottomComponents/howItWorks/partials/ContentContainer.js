33a2812cefd303645c5f3b1b97ada1750bdc8f62
Jahaziel <jahaziel.vaz@gmail.com>
2018-09-10 20:16:23 -0600

    How it works is 70% completed, New modals

---
+++ src/components/landing/partials/bottomComponents/howItWorks/partials/ContentContainer.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const ContentContainer = styled.div`
+  display: inline-block;
+  padding: 1px;
+  overflow: scroll;
+  
+  max-width: 600px;
+  height: 700px;
+  margin: 0 10px;
+`
+
+export default ContentContainer;
