9e78e2140470e29e1ed48d95d1e616b1c54f42d1
Matt Mortensen <mortensenm1205@gmail.com>
2018-10-01 00:53:23 -0600

    making productlist own component

--- src/components/CRM/MemberPortal/subcomponents/MemberPortal.js
+++ src/components/CRM/MemberPortal/subcomponents/MemberPortal.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Products from '../../Product/Products';
-import ProductItem from '../../Product/subcomponents/ProductItem';
+import ProductList from '../../Product/subcomponents/ProductList';
 import SignOutButton from '../../../SignOut';
+import { loadProducts } from '../../../../actions/productData';
 import { connect } from 'react-redux';
 import * as routes from '../../../../constants/routes';
 
 class MemberPortal extends Component {
+
+  componentDidMount() {
+    this.props.getProductData();
+  }
 
   render() {
     const { auth } = this.props;
@@ -15,7 +19,7 @@ class MemberPortal extends Component {
         <h1>Welcome to our Crm</h1>
         <Link to={routes.PROFILE}>Profile</Link> <br />
         <Link to={routes.CART}>Cart</Link> <br />
-        <Products auth={auth} path={routes.PRODUCTS}/>
+        <ProductList {...this.props} path={routes.PRODUCTS}/>
         <SignOutButton />
       </div>
     );
@@ -23,11 +27,15 @@ class MemberPortal extends Component {
 }
 
 const mapStateToProps = state => {
-  return {};
+  return {
+    productData: state.loadingProductData.data
+  };
 };
 
 const mapDispatchToProps = dispatch => {
-  return {}
+  return {
+    getProductData: () => dispatch(loadProducts())
+  }
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(MemberPortal);
--- src/components/CRM/Product/Products.js
+++ src/components/CRM/Product/Products.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import ProductList from './subcomponents/ProductList';
-import ProductItem from './subcomponents/ProductItem';
 import * as routes from '../../../constants/routes';
 import { loadProducts } from '../../../actions/productData';
 import { connect } from 'react-redux';
@@ -13,36 +11,27 @@ class Products extends Component {
   }
 
   render() {
-    const { auth, path } = this.props;
+    const { comp: Component, auth, path } = this.props;
     return(
-      <div>
-        {auth.authenticated ?
-          <div>
-            <ProductList {...this.props} />
-            <Route
-              exact
-              path={`${path}/:productId`}
-              render={rest => auth.authenticated && <ProductItem {...this.props} {...rest} {...auth} /> }
-            />
-          </div>
-          :
-          <p>No products to display</p>
-        }
-        </div>
-      )
-    }
+      <Route
+        exact
+        path={`${path}/:productId`}
+        render={rest => <Component {...this.props} {...rest} {...auth} /> }
+      />
+    )
   }
+}
 
-  export const mapStateToProps = state => {
-    return {
-      productData: state.loadingProductData.data
-    }
+export const mapStateToProps = state => {
+  return {
+    productData: state.loadingProductData.data
   }
+}
 
-  export const mapDispatchToProps = dispatch => {
-    return {
-      getProductData: () => dispatch(loadProducts())
-    }
+export const mapDispatchToProps = dispatch => {
+  return {
+    getProductData: () => dispatch(loadProducts())
   }
+}
 
-  export default connect(mapStateToProps, mapDispatchToProps)(Products);
+export default connect(mapStateToProps, mapDispatchToProps)(Products);
