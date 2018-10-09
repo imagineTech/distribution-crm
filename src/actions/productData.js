733d72c39ddc9d178bfa5f1bc3c1ae7b32fec4a5
Matt Mortensen <mortensenm1205@gmail.com>
2018-10-09 10:26:47 -0600

    bringing in image actions

--- src/actions/productData.js
+++ src/actions/productData.js
@@ -7,10 +7,22 @@ export const loadProductData = (prodData) => {
   }
 }
 
+export const loadProductImageData = (imageProductData) => {
+  return {
+    type: "LOAD_PRODUCT_IMAGE",
+    payload: imageProductData
+  }
+}
+
 export const loadProducts = () => {
   return dispatch => {
     Moltin.getAllProducts().then(products => {
       dispatch(loadProductData(products));
     })
+  }
+} 
+
+export const loadProductImage = () => {
+  return dispatch => {
   }
 }
