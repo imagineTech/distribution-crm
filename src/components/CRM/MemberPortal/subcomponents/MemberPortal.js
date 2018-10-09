b7671f69da48b5b73e512fdecb63b3ef349b313d
Matt Mortensen <mortensenm1205@gmail.com>
2018-10-09 11:19:45 -0600

    MemberPortal now presentational

--- src/components/CRM/MemberPortal/subcomponents/MemberPortal.js
+++ src/components/CRM/MemberPortal/subcomponents/MemberPortal.js
@@ -1,24 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import ProductList from '../../Product/subcomponents/ProductList';
 import SignOutButton from '../../../SignOut';
 import * as routes from '../../../../constants/routes';
 
-class MemberPortal extends Component {
-
-  render() {
-    return (
-      <div>
-        <h1>Welcome to our Crm</h1>
-        <Link to={routes.PROFILE}>Profile</Link> <br />
-        <Link to={routes.CART}>Cart</Link> <br />
-        <ProductList {...this.props} path={routes.PRODUCTS}/>
-        <SignOutButton />
-      </div>
-    );
-  };
+const MemberPortal = props => {
+  return (
+    <div>
+      <h1>Welcome to our Crm</h1>
+      <Link to={routes.PROFILE}>Profile</Link> <br />
+      <Link to={routes.CART}>Cart</Link> <br />
+      <ProductList {...props} path={routes.PRODUCTS}/>
+      <SignOutButton />
+    </div>
+  );
 }
-
-
 
 export default MemberPortal;
