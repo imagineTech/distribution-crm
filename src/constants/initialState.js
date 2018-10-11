a7872bd008f1689a06d7903880af4666ba86e422
Matt Mortensen <mortensenm1205@gmail.com>
2018-10-10 20:11:27 -0600

    updating initial

--- src/constants/initialState.js
+++ src/constants/initialState.js
@@ -1,10 +1,7 @@
  const loadingProductData = {
     data: [
     {
-        commodity_type: "physical",
-        description: "A second product",
         id: "11adb0a9-40e6-4f54-8052-05b5edad8970",
-        manage_stock: true,
         meta:
         {
             display_price:
@@ -21,9 +18,6 @@
             stock:
             {availability: "in-stock",
             level: 99999},
-            timestamps:
-            { created_at: "2018-10-09T00:00:55+00:00",
-            updated_at: "2018-10-09T18:27:46+00:00"},
             name: "Test Product 2"
         },
         price: [
@@ -38,9 +32,6 @@
             {data:
             {id: "e77e681a-07a8-4082-b876-0f4db36b7780",
             type: "main_image"}}},
-        sku: "002",
-        slug: "testProductTwo",
-        status: "live",
         type: "product"
     }
     ]
