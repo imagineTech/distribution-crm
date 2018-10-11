e749a9e72cdd8e3c541b1f5db06281038c2969e9
Matt Mortensen <mortensenm1205@gmail.com>
2018-10-11 10:56:49 -0600

    index now supports createTheStore func

--- src/index.js
+++ src/index.js
@@ -1,52 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createTheStore } from './store';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react'
-import storage from 'redux-persist/lib/storage';
-import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import { ConnectedRouter } from 'connected-react-router';
-import { createBrowserHistory } from 'history';
-import { connectRouter, routerMiddleware } from 'connected-react-router';
 import { Elements, StripeProvider } from 'react-stripe-elements';
 import { _stripeKey } from './stripe/config_stripe';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers/index';
-import { initialState } from './constants/initialState';
 import App from './containers/App';
 
-//Had to combine my config_store file with this index file
-//just because i needed the history obj to work with
-//my redux action calls.
-const initial = {
-  loadingProductData: initialState.loadingProductData
-}
-const history = createBrowserHistory();
-const persistConfig = {
-  key: 'root',
-  storage,
-  stateReconciler: autoMergeLevel2, 
-  blacklist: [
-    'storeNewProfileData', 
-    'signUpFormData', 
-    'emailAndPasswordData',
-  ]
-};
-const store = createStore(
-  //for my redux data to stay, had to bring in redux-persist
-  //and combine it with my connect-react-router. This one line
-  //is still consider the rootReducer argument.
-  persistReducer(persistConfig,(connectRouter(history)(rootReducer))),
-  initial,
-  compose(
-    applyMiddleware(
-      thunk,
-      routerMiddleware(history)
-    ),
-  ),
-);
-const persistor = persistStore(store);
+const store = createTheStore().store;
+const history = createTheStore().history;
+const persistor = createTheStore().persistor;
 
 render(
   <Provider store={store}>
