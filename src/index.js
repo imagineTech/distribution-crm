e30dd973a4fa4ac5973a34f327d61f62f48dc1e5
Matt Mortensen <mortensenm1205@gmail.com>
2018-10-12 11:10:04 -0600

    removed connected-react-router :)

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { history, creatingTheStore } from './store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react'
-import { ConnectedRouter } from 'connected-react-router';
+import { Router } from 'react-router-dom';
 import { Elements, StripeProvider } from 'react-stripe-elements';
 import { _stripeKey } from './stripe/config_stripe';
 import App from './containers/App';
@@ -14,13 +14,13 @@ const persistor = creatingTheStore().persistor;
 render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <ConnectedRouter history={history}>
+      <Router history={history}>
         <StripeProvider apiKey={_stripeKey}>
           <Elements>
             <App storeToTest={store}/>
           </Elements>
         </StripeProvider>
-      </ConnectedRouter>
+      </Router>
     </PersistGate>
   </Provider>,
   document.getElementById('root')
