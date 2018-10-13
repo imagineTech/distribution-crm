96d2a77ae209b4a30b22ad64ef706277fd00ebc2
Matt Mortensen <mortensenm1205@gmail.com>
2018-10-13 11:34:43 -0600

    passing history to signOut

--- src/components/profile/subcomponents/Profile.js
+++ src/components/profile/subcomponents/Profile.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 import SignOutButton from '../../SignOut';
 import * as routes from '../../../constants/routes';
 
-const Profile = ({ profileData, match }) => {
+const Profile = ({ profileData, match, history }) => {
   return(
     <div>
       <h1>{`${profileData.First_Name} ${profileData.Last_Name}`}</h1>
       <p>Email: {profileData.Email}</p>
       <button><Link to={`${match.url}/${routes.EDIT_PROFILE}`}>Edit Profile</Link></button>
-      <SignOutButton />
+      <SignOutButton history={history} />
     </div>
   )
 }
