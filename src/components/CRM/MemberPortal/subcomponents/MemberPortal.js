7ad3237346ee81a18ecf7d39507fd3164c44d7b6
Matt Mortensen <mortensenm1205@gmail.com>
2018-10-13 20:00:38 -0600

    removing async from MemberPro

--- src/components/CRM/MemberPortal/subcomponents/MemberPortal.js
+++ src/components/CRM/MemberPortal/subcomponents/MemberPortal.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import asyncComponent from '../../../../hoc/async';
 import { Link } from 'react-router-dom';
+import Profile from '../../../profile/subcomponents/Profile';
+import ProductList from '../../Product/subcomponents/ProductList';
 import SignOutButton from '../../../SignOut';
 import * as routes from '../../../../constants/routes';
-
-const AsyncProfile = asyncComponent(() => {
-  return import('../../../profile/subcomponents/Profile');
-});
-const AsyncProductList = asyncComponent(() => {
-  return import('../../Product/subcomponents/ProductList');
-});
 
 const MemberPortal = props => {
   return (
     <div>
       <h1>Welcome to our Crm</h1>
-      <AsyncProfile {...props} path={routes.PROFILE}/> <br />
+      <Profile {...props} path={routes.PROFILE}/> <br />
       <Link to={routes.CART}>Cart</Link> <br />
-      <AsyncProductList {...props} path={routes.PRODUCTS}/>
+      <ProductList {...props} path={routes.PRODUCTS}/>
       <SignOutButton history={props.history}/>
     </div>
   );
