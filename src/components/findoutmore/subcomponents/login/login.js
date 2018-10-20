a48bb04af6423495249ee83c7169aec44e575459
Megan Callister <meganecallister@gmail.com>
2018-10-19 19:49:59 -0600

    merge changes?

--- src/components/findoutmore/subcomponents/login/login.js
+++ src/components/findoutmore/subcomponents/login/login.js
@@ -20,16 +20,16 @@ class Login extends Component {
   }
 
   handleSubmit = e => {
-    const { emailAndPassword, emailAndPasswordLogin } = this.props;
+    const { emailAndPassword, emailAndPasswordLogin, history } = this.props;
     const { login_email, login_password} = emailAndPassword;
     e.preventDefault();
-    emailAndPasswordLogin(login_email, login_password);
+    emailAndPasswordLogin(login_email, login_password, history);
   }
 
   render() {
     return (
       <div>
-				<FontAwesomeIcon id="close-login-modal" onClick={this.props.closeLoginModal} icon='window-close' style={{height: 25, width: 25}}/>
+	      <FontAwesomeIcon id="close-login-modal" onClick={this.props.closeLoginModal} icon='window-close' style={{height: 25, width: 25}}/>
 
         <p className='modal-title' id='login'>Login</p>
 
@@ -46,9 +46,8 @@ class Login extends Component {
             <input type="password" id="login_password" name="login_password" onChange={this.handleChange} />
           </label>
           <br/>
-          <Link to={routes.MEMBER_PORTAL}>
-            <button>Login</button>
-          </Link>
+
+          <button>Login</button>
 
         </form>
         <Link to={routes.SIGN_UP}>Don't have an account? Signup</Link>
@@ -66,7 +65,7 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
   return {
     emailAndPasswordChange: (name, value) => dispatch(dataToLoginWith(name, value)),
-    emailAndPasswordLogin: (email, password) => dispatch(loginWithEmailAndPassword(email, password))
+    emailAndPasswordLogin: (email, password, history) => dispatch(loginWithEmailAndPassword(email, password, history))
   }
 }
 
