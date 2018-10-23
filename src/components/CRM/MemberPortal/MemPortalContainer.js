ffa979000a1790aa454ac2324c59e024536567d4
Shubham Tiwari <websbiz0000@gmail.com>
2018-10-23 15:44:21 +0000

    can add and change profile photos

--- src/components/CRM/MemberPortal/MemPortalContainer.js
+++ src/components/CRM/MemberPortal/MemPortalContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MemberPortal from './subcomponents/MemberPortal';
+import {database} from '../../../firebase/config_firebase';
 import { loadProfileData } from '../../../actions/profileData';
 import { loadProducts, loadProductImage } from '../../../actions/productData';
 import { connect } from 'react-redux';
@@ -7,17 +8,35 @@ import { connect } from 'react-redux';
 
 
 class MemberPortalContainer extends Component {
-
+  state = {url:''}
   componentDidMount() {
     const { getProductData, getProductImage, getProfileData, auth } = this.props;
     getProductData();
     getProductImage();
     getProfileData(auth.uid)
+    console.log('uid',auth.authUser);
+    let uid = auth.authUser.uid;
+    database.ref('/profile_images/'+uid).once('value').then(snapshot => {
+      
+      let profile_photos = snapshot.val();
+      
+        let photo = !!profile_photos?Object.keys(profile_photos).map(key => profile_photos[key]):[{url:''}]
+        let arr = photo[photo.length-1]
+        this.setState({url:arr.url})  
+      
+    })
+    database.ref('/profile_images/'+uid).on('child_added', snapshot => {
+      let profile_photos = snapshot.val();
+      let photo = !!profile_photos?Object.keys(profile_photos).map(key => profile_photos[key]):[{url:''}]
+        let arr = photo[photo.length-1]
+        this.setState({url:arr.url})  
+    })
+    
   }
 
   render() {
     const { rest } = this.props;
-    return <MemberPortal {...this.props} {...rest} />
+    return <MemberPortal {...this.props} {...rest} url={this.state.url} uid={this.props.auth.authUser.uid}/>
   }
 }
 
--- src/components/CRM/MemberPortal/subcomponents/MemberPortal.js
+++ src/components/CRM/MemberPortal/subcomponents/MemberPortal.js
@@ -5,16 +5,17 @@ import ProductList from '../../Product/subcomponents/ProductList';
 import SignOutButton from '../../../SignOut';
 import * as routes from '../../../../constants/routes';
 
-const MemberPortal = props => {
+class MemberPortal extends React.Component {
+  render(){
   return (
     <div>
       <h1>Welcome to our Crm</h1>
-      <Profile {...props} path={routes.PROFILE}/> <br />
-      <SignOutButton history={props.history}/>
+      <Profile {...this.props} path={routes.PROFILE} url={this.props.url} uid={this.props.uid}/> <br />
+      <SignOutButton history={this.props.history}/>
       <Link to={routes.CART}>Cart</Link> <br />
-      <ProductList {...props} path={routes.PRODUCTS}/>
+      <ProductList {...this.props} path={routes.PRODUCTS}/>
     </div>
   );
 }
-
+}
 export default MemberPortal;
