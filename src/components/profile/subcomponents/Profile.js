a5b15969e2ce86fea926e361324da8a3bf6ffa5c
Reid South <rbsouth@gmail.com>
2018-10-23 21:27:01 -0600

    minimal styling done on profile component

--- src/components/profile/subcomponents/Profile.js
+++ src/components/profile/subcomponents/Profile.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Profile.css'
 import * as routes from '../../../constants/routes';
 
 const Profile = ({ profileData, path }) => {
   return(
     <div>
+      <img class="profilepicture" src="https://dummyimage.com/200x100/000/cccccc&text=Profile+Picture"></img>
       <h1>{`${profileData.First_Name} ${profileData.Last_Name}`}</h1>
       <p>Email: {profileData.Email}</p>
       <button><Link to={`${path}/${routes.EDIT_PROFILE}`}>Edit Profile</Link></button>
