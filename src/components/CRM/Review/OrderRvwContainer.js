856ed1239e6c06dc1914815cad9b3f553882eafe
Reid South <rbsouth@gmail.com>
2018-10-23 22:51:03 -0600

    commit before update

--- src/components/CRM/Review/OrderRvwContainer.js
+++ src/components/CRM/Review/OrderRvwContainer.js
@@ -4,18 +4,15 @@ import { connect } from 'react-redux';
 
 class OrderRvwContainer extends Component {
 
-<<<<<<< HEAD
   componentDidMount() {
     const { getOrder, profileData } = this.props;
     const { Orders } = profileData
     getOrder(Orders[Orders.length - 1].id);
   }
 
-=======
->>>>>>> 3059cd3472833ee80bb2e15bd0e5aabad0574bb5
   render() {
     const { rest } = this.props;
-    return <OrderReview {...this.props} {...rest} /> 
+    return <OrderReview {...this.props} {...rest} />
   }
 }
 
