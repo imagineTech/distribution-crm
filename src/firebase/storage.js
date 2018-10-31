e90446acac061a15d75378ce40a6c9ff2124c58f
Shubham Tiwari <websbiz0000@gmail.com>
2018-10-31 15:47:58 +0000

    adding progressbar to make site look more cool during async

--- src/firebase/storage.js
+++ src/firebase/storage.js
@@ -1,6 +1,8 @@
 import { storage } from './config_firebase';
+import NProgress from 'nprogress';
 
 export const uploadImage = (userId, imgFile) => {
+    NProgress.start();
     return storage.child('imgs/' + userId).put(imgFile)
 }
 
