6fb85b28f878c264680a2db4d0fe0b9c4dff5581
Matt Mortensen <mortensenm1205@gmail.com>
2018-10-30 18:55:07 -0600

    writing storage methods

--- src/firebase/storage.js
+++ src/firebase/storage.js
@@ -1,1 +1,9 @@
 import { storage } from './config_firebase';
+
+export const uploadImage = (userId, imgFile) => {
+    return storage.ref().child('images/' + userId).put(imgFile)
+}
+
+export const downloadImage = userId => {
+    return storage.ref().child('images/' + userId).getDownloadURL()
+}
