34e7795e0627ffad6cbb766a9ec4c60552d26737
Matt Mortensen <mortensenm1205@gmail.com>
2018-11-07 22:43:34 -0700

    testing removal of cart propType errs

--- src/components/CRM/Cart/CartContainer.js
+++ src/components/CRM/Cart/CartContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Cart from './subcomponents/Cart';
+import { loadProfileData } from '../../../actions/profileData';
 import { decrementStock } from '../../../actions/productData';
 import { loadCart, updateCartItemQty, checkOutCart,removingCartItem, deleteCart } from '../../../actions/cartData';
 import { addOrderDataToStore } from '../../../actions/orderData';
@@ -9,8 +10,10 @@ import PropTypes from 'prop-types';
 class CartContainer extends Component {
 
   componentDidMount() {
-    const { profileData, getCartData } = this.props;
+    const { profileData, getProfileData, getCartData, auth } = this.props;
+    getProfileData(auth.authUser.uid);
     getCartData(profileData.id);
+    console.log(profileData)
   }
 
   render() {
@@ -29,6 +32,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
+    getProfileData: userId => dispatch(loadProfileData(userId)),
     getCartData: (crtId) => dispatch(loadCart(crtId)),
     updateQty: (cartId, itemId, newQty) => dispatch(updateCartItemQty(cartId, itemId, newQty)),
     removeItm: (cartId, itemId, qty) => dispatch(removingCartItem(cartId, itemId, qty)),
@@ -44,7 +48,6 @@ CartContainer.propTypes = {
     id: PropTypes.string.isRequired
   }).isRequired,
   getCartData: PropTypes.func.isRequired,
-  rest: PropTypes.object.isRequired
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartContainer);
