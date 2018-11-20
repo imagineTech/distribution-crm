87a363265bad69b732e0e6e315b33464d92fb500
seankidwell <dev.seankidwell@gmail.com>
2018-11-19 22:12:37 -0700

    beginnings of order confirmation email cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +8,12 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
 
+exports.sendOrderConfirmation = functions.firestore
+  .document('Buyer/{BuyerId}').onUpdate((change, context) => {
+    console.log('@@@@@@@ Firestore Updated @@@@@@@@')
+    let preUpdate = change.before.data();
+    let postUpdate = change.after.data();
+    preUpdate.Orders && (preUpdate.Orders.length!==postUpdate.Orders.length) || (!preUpdate.Orders && postUpdate.Orders)?
+      console.log(postUpdate.Orders[postUpdate.Orders.length-1]):
+      null
+  })
